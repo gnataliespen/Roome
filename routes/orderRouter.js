2e47587dbe458f8a8ae302a888a602830a30089a
Natalie Spencer <gnataliespen@gmail.com>
2019-11-22 22:27:05 -0600

    Client fix

--- routes/orderRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/orderController");
-const auth = require("../middleware/auth");
-
-router.get("/", auth, controller.getOrders);
-
-module.exports = router;
