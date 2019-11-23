663bfa5852083ccb37ac736e0b7efcf34b544af8
gnataliespen <44278932+gnataliespen@users.noreply.github.com>
2019-11-22 23:03:05 -0600

    Update apiConnection.js

--- client/src/util/apiConnection.js
+++ client/src/util/apiConnection.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-let url =
-  process.env.NODE_ENV === "production"
-    ? "https://roome-backend.herokuapp.com/"
-    : "http://localhost:2000/";
-
 export default axios.create({
-  baseURL: url
+  baseURL: "https://roome-backend.herokuapp.com/"
 });
